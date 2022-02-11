bb412f11d84bfa9f2214526aeb35a96d4835b93d
syamil <syamil@bearylogical.net>
2022-02-11 17:15:26 +0100

    add experiment tracking

---
+++ src/utils/experiment.py
@@ -0,0 +1,11 @@
+from tensorflow.keras.callbacks import Callback
+import wandb
+
+class LRLogger(Callback):
+    def __init__(self, optimizer):
+      super(LRLogger, self).__init__()
+      self.optimizer = optimizer
+
+    def on_epoch_end(self, epoch, logs):
+      lr = self.optimizer.learning_rate(self.optimizer.iterations)
+      wandb.log({"lr": lr}, commit=False)
