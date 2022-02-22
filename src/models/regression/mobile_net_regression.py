67d5a4167ea61a979a13deec2ad74939ff262504
syamil <syamil@bearylogical.net>
2022-02-22 16:54:11 +0100

    add mobile net backbone

---
+++ src/models/regression/mobile_net_regression.py
@@ -0,0 +1,40 @@
+from tensorflow.keras.applications import MobileNetV2
+from tensorflow.keras.layers import GlobalAveragePooling2D, Concatenate, Dense, Resizing
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+from tensorflow.keras import Model, Input
+
+# transfer learning demo
+
+class MobleNetRegresssion(Model):
+    def __init__(self, num_target=4, img_size:tuple=(128,128)):
+        """
+        Base Model for image regression
+
+        :param num_target: Number of target features as an output 1D vector
+        :param img_size: Height x Width of input image.
+        """
+        super(MobleNetRegresssion, self).__init__()
+        self.concat = Concatenate()
+        self.preprocess_resize = Resizing(*img_size, crop_to_aspect_ratio=True)
+        self.mobile_net = MobileNetV2(input_shape=img_size + (3,), alpha=1.0, include_top=False, weights="imagenet")
+        self.mobile_net.trainable = False
+        self.avgpool1 = GlobalAveragePooling2D()
+        self.dense3 = Dense(num_target)
+        self.num_target = num_target
+
+    def call(self, inputs, training=None, mask=None):
+        x = self.concat([inputs, inputs, inputs])
+        x = self.preprocess_resize(x)
+        x = preprocess_input(x)
+        x = self.mobile_net(x)
+        x = self.avgpool1(x)
+        return self.dense3(x)
+
+
+if __name__ == "__main__":
+    aa = MobleNetRegresssion()
+    model_input = Input((128, 128, 1))
+    aa(model_input)
+    aa.build(input_shape=(None, 128, 128, 1))
+    aa.call(model_input)
+    aa.summary()
