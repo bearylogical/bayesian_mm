a2ba48fe2b60c5a13e7f29074327001252eca82b
syamil <bearylogical@duck.com>
2022-02-28 19:34:51 +0100

    amend default cnn

--- src/models/regression/cnn_regression.py
+++ src/models/regression/cnn_regression.py
@@ -3,7 +3,7 @@ from keras.layers import Conv2D, Dense, Flatten, MaxPooling2D, Resizing
 
 
 class ImageRegressionModel(Model):
-    def __init__(self, num_target=4, img_size:tuple=(128,128)):
+    def __init__(self, num_target=4, img_size: tuple = (128, 128)):
         """
         Base Model for image regression
 
@@ -13,7 +13,7 @@ class ImageRegressionModel(Model):
         super().__init__()
         self.preprocess_resize = Resizing(*img_size, crop_to_aspect_ratio=True)
         self.conv_1 = Conv2D(32, (3, 3), activation='relu', input_shape=(128, 128, 1))
-        self.pool_1 = MaxPooling2D((5,5))
+        self.pool_1 = MaxPooling2D((3, 3))
         self.conv_2 = Conv2D(16, 3, activation='relu')
         self.pool_2 = MaxPooling2D((3, 3))
         self.flatten_1 = Flatten()
@@ -40,6 +40,7 @@ if __name__ == "__main__":
 
     import random
     from src.utils.loader import get_image_paths_from_dir, RegressionDataLoader
+
     num_train = 800
 
     img_data_dir = "../../../dataset/20220209/images"
@@ -52,11 +53,9 @@ if __name__ == "__main__":
     train_img_paths = img_paths[:num_train]
     test_img_paths = img_paths[num_train:]
 
-    gen_kwargs = dict(target_data_path=target_data_path, num_targets=4, batch_size=batch_size, img_size=(128,128))
+    gen_kwargs = dict(target_data_path=target_data_path, num_targets=4, batch_size=batch_size, img_size=(128, 128))
 
     train_gen = RegressionDataLoader(input_img_paths=train_img_paths, **gen_kwargs)
     test_gen = RegressionDataLoader(input_img_paths=test_img_paths, **gen_kwargs)
 
     # train_sample
-
-
