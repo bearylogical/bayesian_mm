5b7a95d70dc6549b39cb4f26a2fcd236bc6edb8b
syamil <bearylogical@duck.com>
2022-03-02 16:52:59 +0100

    update defaults

--- src/utils/augmentations.py
+++ src/utils/augmentations.py
@@ -2,12 +2,14 @@ import albumentations as A
 
 def default_aug():
     return A.Compose([
-        A.RandomBrightnessContrast(p=0.2),
-        A.ShiftScaleRotate(shift_limit=0.05, scale_limit=0.05, rotate_limit=30, p=0.3),
-        A.HorizontalFlip(),
-        A.GaussianBlur(p=0.8),
-        A.GaussNoise(p=0.4, var_limit=(0.02, 0.04)),
-        A.RandomBrightnessContrast(p=0.5),
+        A.ShiftScaleRotate(shift_limit=0.05, scale_limit=0.05, rotate_limit=10, p=0.5),
+        A.GaussianBlur(p=0.5),
+        A.GaussNoise(p=0.5, var_limit=(0.002, 0.003)),
+        A.RandomBrightnessContrast(p=0.5, contrast_limit=0.5, brightness_limit=0.1),
 
     ], keypoint_params=A.KeypointParams(format='xy', remove_invisible=False, angle_in_degrees=True))
 
+# def field_of_view():
+#     return A.Compose([
+#         A.Crop(x_min=x_min, x_max=x_max, y_min=y_min, y_max=y_max
+#     ],keypoint_params=A.KeypointParams(format='xy', remove_invisible=False, angle_in_degrees=True))
