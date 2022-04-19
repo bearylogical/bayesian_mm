fix scaling of wanb callback

src/models/evaluate.py
@@ -2,7 +2,7 @@ from keras import Model, models
 from tensorflow.keras.utils import Sequence
 from PIL import Image
 import numpy as np
-from src.utils.loader import KeyPointDataLoader, match_image_to_target, get_keypoint_from_ls
+from src.utils.loader import KeyPointDataLoader, match_image_to_target, get_keypoint_dict_from_ls, rescale_kps_from_pct
 from tqdm import tqdm
 import wandb
 from keras.callbacks import Callback
@@ -33,8 +33,8 @@ class LogImagePredictionPerNumEpochs(Callback):
 
             # dataloader normalizes intensities between 0 and 1
             _img_arr = data[0][img_idx_per_batch].squeeze() * 255.0
-            scaled_prediction = get_keypoint_from_ls(_img_arr.shape, list(prediction))
-            scaled_ground_truth = get_keypoint_from_ls(_img_arr.shape, list(prediction))
+            scaled_prediction = np.array(rescale_kps_from_pct(_img_arr.shape, list(prediction)))
+            scaled_ground_truth = np.array(rescale_kps_from_pct(_img_arr.shape, list(prediction)))
             # wandb specific code
             _original_img = Image.fromarray(_img_arr)
             _wandb_original_image = wandb.Image(_original_img.convert("RGB"))

src/utils/loader.py
@@ -282,15 +282,20 @@ def get_target_data_from_idx(data: np.ndarray, img_idx: int, include_idx=False,
         return f_data[fields].item()
 
 
-def get_keypoint_from_ls(img_shape: tuple, kp_list: list):
+def get_keypoint_dict_from_ls(img_shape: tuple, kp_list: list) -> dict:
+    _rescaled_kps = rescale_kps_from_pct(img_shape, kp_list)
+    return {f"x{idx // 2}" if idx % 2 == 0 else f"y{idx // 2}": v for idx, v in enumerate(_rescaled_kps)}
+
+
+def rescale_kps_from_pct(img_shape: tuple, kp_list: list) -> list:
     h_factor, w_factor = (v / 100 for v in img_shape)
-    kps = {}
+    _rescaled_kps = []
     for idx, v in enumerate(kp_list):
         if idx % 2 == 0:
-            kps[f"x{idx // 2}"] = v * w_factor
+            _rescaled_kps.append(v * w_factor)
         else:
-            kps[f"y{idx // 2}"] = v * h_factor
-    return kps
+            _rescaled_kps.append(v * h_factor)
+    return _rescaled_kps
 
 
 def get_img_target_data(img_path: Union[str, Path],
@@ -327,7 +332,7 @@ def get_img_target_data(img_path: Union[str, Path],
 
     elif data_path_ext == ".json":
         kps_list = get_keypoints_from_json(json.load(open(data_path)), 14)
-        return img, get_keypoint_from_ls(img.shape, kps_list)
+        return img, get_keypoint_dict_from_ls(img.shape, kps_list)
 
     else:
         raise Exception(f"Invalid extension of data path {data_path_ext}")

src/utils/viewer.py
@@ -12,7 +12,7 @@ import cv2
 import albumentations as A
 import numpy as np
 from src.utils.loader import get_image_paths_from_dir, RegressionDataLoaderT1, get_img_target_data, \
-    match_image_to_target, get_keypoint_from_ls, KeyPointDataLoader
+    match_image_to_target, get_keypoint_dict_from_ls, KeyPointDataLoader
 from src.utils.utilities import prepare_img_prediction
 from PIL import Image
 from typing import Union, List, Tuple
@@ -248,7 +248,7 @@ def display_keypoints_prediction(model: Model,
                                  true_keypoints: np.ndarray = None):
     img_arr = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
     pred_coords = model.predict(prepare_img_prediction(img_arr))
-    rescale_pred_coords = np.array(list(get_keypoint_from_ls(img_arr.shape, list(pred_coords[0])).values()))
+    rescale_pred_coords = np.array(list(get_keypoint_dict_from_ls(img_arr.shape, list(pred_coords[0])).values()))
     # TODO: Refactor as ImageModel method
     display_img_coords(img_arr, true_keypoints, rescale_pred_coords)
 