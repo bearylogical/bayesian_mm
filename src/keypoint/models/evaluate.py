238bb4f72bb70b0b87028c2472322d3ff873b006
syamil <bearylogical@duck.com>
2022-06-28 22:27:47 +0200

    fix evaluate for hetero model

--- src/keypoint/models/evaluate.py
+++ src/keypoint/models/evaluate.py
@@ -2,11 +2,16 @@ from keras import Model, models
 from tensorflow.keras.utils import Sequence
 from PIL import Image
 import numpy as np
-from src.utils.dataloader import KeyPointDataLoader, match_image_to_target, rescale_kps_from_pct
+from src.utils.dataloader import (
+    KeyPointDataLoader,
+    match_image_to_target,
+    rescale_kps_from_pct,
+)
 import wandb
 from keras.callbacks import Callback
 
 from src.utils.viewer import show_image_coords
+from src.utils.constants import NUM_TARGETS
 
 
 class LogImagePredictionPerNumEpochs(Callback):
@@ -19,11 +24,20 @@ class LogImagePredictionPerNumEpochs(Callback):
         self._create_predictions_table()
 
     def _create_predictions_table(self):
-        columns = ["id", "epoch", "img_num", "image", "predictions", "truth", "loss", "run_id"]
+        columns = [
+            "id",
+            "epoch",
+            "img_num",
+            "image",
+            "predictions",
+            "truth",
+            "loss",
+            "run_id",
+        ]
         self.predictions_table = wandb.Table(columns=columns)
 
     def _log_predictions(self, data_loader: Sequence, epoch):
-        predictions = self.model.predict(data_loader)
+        predictions = self.model.predict(data_loader)[:, :NUM_TARGETS]
         img_idx_per_batch = 3
         for batch_idx, data in enumerate(data_loader):
             img_id = str(batch_idx) + "_" + str(epoch)
@@ -33,25 +47,38 @@ class LogImagePredictionPerNumEpochs(Callback):
 
             # dataloader normalizes intensities between 0 and 1
             _img_arr = data[0][img_idx_per_batch].squeeze() * 255.0
-            scaled_prediction = np.array(rescale_kps_from_pct(_img_arr.shape, list(prediction)))
-            scaled_ground_truth = np.array(rescale_kps_from_pct(_img_arr.shape, list(ground_truth)))
+            scaled_prediction = np.array(
+                rescale_kps_from_pct(_img_arr.shape, list(prediction))
+            )
+            scaled_ground_truth = np.array(
+                rescale_kps_from_pct(_img_arr.shape, list(ground_truth))
+            )
             # wandb specific code
             _original_img = Image.fromarray(_img_arr)
             _wandb_original_image = wandb.Image(_original_img.convert("RGB"))
-            image_kwargs = {
-                "radius": 1,
-                "xy_offset": (3, 1),
-                "font_size": 7
-            }
+            image_kwargs = {"radius": 1, "xy_offset": (3, 1), "font_size": 7}
             _wandb_ground_truth = wandb.Image(
-                show_image_coords(_img_arr, true_coords=scaled_ground_truth, **image_kwargs))
+                show_image_coords(
+                    _img_arr, true_coords=scaled_ground_truth, **image_kwargs
+                )
+            )
             _wandb_predicted_image = wandb.Image(
-                show_image_coords(_img_arr, pred_coords=scaled_prediction, **image_kwargs))
+                show_image_coords(
+                    _img_arr, pred_coords=scaled_prediction, **image_kwargs
+                )
+            )
             _loss = np.linalg.norm(data[1][img_idx_per_batch] - prediction, axis=0)
 
-            self.predictions_table.add_data(img_id, epoch, img_num, _wandb_original_image, _wandb_predicted_image,
-                                            _wandb_ground_truth,
-                                            _loss, wandb.run.id)
+            self.predictions_table.add_data(
+                img_id,
+                epoch,
+                img_num,
+                _wandb_original_image,
+                _wandb_predicted_image,
+                _wandb_ground_truth,
+                _loss,
+                wandb.run.id,
+            )
 
     def on_epoch_end(self, epoch, logs=None):
         if epoch % self.predict_every == 0:
@@ -67,27 +94,45 @@ def evaluate(model: Model, test_loader: KeyPointDataLoader):
     """
 
     loss = model.evaluate(test_loader)
-    highest_losses, hardest_examples, true_labels, predictions, hardest_k_names = get_hardest_k_examples(model, test_loader)
+    (
+        highest_losses,
+        hardest_examples,
+        true_labels,
+        predictions,
+        hardest_k_names,
+    ) = get_hardest_k_examples(model, test_loader)
 
-    return loss, highest_losses, hardest_examples, true_labels, predictions, hardest_k_names
+    return (
+        loss,
+        highest_losses,
+        hardest_examples,
+        true_labels,
+        predictions,
+        hardest_k_names,
+    )
 
 
-def get_hardest_k_examples(model: Model,
-                           test_loader: KeyPointDataLoader, k=10):
+def get_hardest_k_examples(model: Model, test_loader: KeyPointDataLoader, k=10):
     # losses = np.zeros(len(test_loader))
 
     predictions = model.predict(test_loader, verbose=0)
     truths = np.array([test_loader[idx][1][0] for idx in range(len(test_loader))])
     losses = np.linalg.norm(predictions - truths, axis=1)
-    argsort_loss = np.argsort(losses) # indices of sorted losses
-    top_k_loss = argsort_loss[-k:] # top K loss indices
+    argsort_loss = np.argsort(losses)  # indices of sorted losses
+    top_k_loss = argsort_loss[-k:]  # top K loss indices
     highest_k_losses = losses[top_k_loss]
 
     hardest_k_names = [test_loader.input_img_paths[idx].name for idx in top_k_loss]
     hardest_k_examples = [test_loader[top_idx][0] for top_idx in top_k_loss]
     true_labels = [test_loader[top_idx][1] for top_idx in top_k_loss]
     predictions = [model.predict(example) for example in hardest_k_examples]
-    return highest_k_losses, hardest_k_examples, true_labels, predictions, hardest_k_names
+    return (
+        highest_k_losses,
+        hardest_k_examples,
+        true_labels,
+        predictions,
+        hardest_k_names,
+    )
 
 
 if __name__ == "__main__":
@@ -96,6 +141,8 @@ if __name__ == "__main__":
 
     model_path = "models/Baseline_20220417_1725"
     img_model = models.load_model(model_path)
-    val_loader = KeyPointDataLoader(input_img_paths=imgs, target_paths=labels, batch_size=1, img_size=(128, 128))
+    val_loader = KeyPointDataLoader(
+        input_img_paths=imgs, target_paths=labels, batch_size=1, img_size=(128, 128)
+    )
     evaluate(img_model, val_loader)
     # get_hardest_k_examples(img_model, val_loader)
